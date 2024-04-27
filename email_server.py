44887dffce850ff44cd26ebf74510c3659aae9f5
unsubstantiated-Script <jusfulton@gmail.com>
2024-04-27 10:12:51 -0700

    Email w/ tracked image sending

--- email_server.py
+++ email_server.py
@@ -1,14 +1,9 @@
-import os
 import smtplib
 import imghdr
 from email.message import EmailMessage
 
-password = os.getenv("GMAIL_PASSWORD")
-sender = os.getenv("SENDER")
-receiver = os.getenv("RECEIVER")
 
-
-def send_email(image_path):
+def send_email(image_path, password, sender, receiver):
     email_message = EmailMessage()
     email_message["Subject"] = "Intruder Pic"
     email_message.set_content("There's an intruder!")
@@ -25,7 +20,6 @@ def send_email(image_path):
     gmail.sendmail(sender, receiver, email_message.as_string())
     gmail.quit()
 
-
 # Testing method by itself
-if __name__ == "__main__":
-    send_email(image_path="images/test.png")
+# if __name__ == "__main__":
+#     send_email(image_path="images/test.png")
